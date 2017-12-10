632c9ee96f0d37835b3f094171619eefe78365fe
MateuszD <mateuszdeptula1@gmail.com>
2017-12-10 10:49:24 +0100

    zbudowanie klas modelu user i tweet

--- src/main/java/com/mojafirma/model/Tweet.java
+++ src/main/java/com/mojafirma/model/Tweet.java
@@ -1,4 +1,29 @@
 package com.mojafirma.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Tweet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "tweet_id")
+    private int tweetId;
+
+    @Column (name = "tweet_Time")
+    private LocalDateTime tweetDateTime;
+
+    @Column
+    private String message;
+
 }
--- src/main/java/com/mojafirma/model/User.java
+++ src/main/java/com/mojafirma/model/User.java
@@ -1,4 +1,30 @@
 package com.mojafirma.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue
+    @Column (name = "user_id")
+    private int userId;
+
+    @Column (name = "user_name")
+    private String userName;
+
+    @Column (name = "user_password")
+    private String userPassword;
 }
